d448210d6ee03220b5859329104337a263a86080
FrankFr <f.freisberg@telekom.de>
2020-03-21 20:27:21 +0100

    ok, aber ohne funktionierenden Neustart

--- Datumerweiterung.java
+++ Datumerweiterung.java
@@ -8,19 +8,24 @@ public class Datumerweiterung {
 	public static int s;   
 	public static int m1;  
 	public static int m2;  
-	public static int min;	// Minute temp 
+	public static int mintemp;	// Minute temp 
 	
-	int[] monlaeng = new int[]  {31, 28, 31,   30, 31, 30,  	31, 31, 30,   31, 30, 31};
-
+	static String eingabe;
+	static boolean calc = false;
+	
+	static int[] monlaeng = new int[]  {31, 28, 31,   30, 31, 30,  	31, 31, 30,   31, 30, 31};
+	
 	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+		do  {
 
 		System.out.println("Diese Programm addiert zu einem Datum eine bestimmte Menge an Minuten");
 		System.out.println();
-		System.out.println("Bitte geben sie ein Datum im Format Jahr Monat Tag Stunde Minute mit Leerzeichen ein");
-				
-		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("Bitte geben sie ein Datum im Format 1.Jahr  2.Monat  3.Tag  4.Stunde  5.Minute  6.Minuten-Summand   mit Leerzeichen ein");
+			
 		j = sc.nextInt();   	// Jahr
 		m = sc.nextInt();		// Monat
 		t = sc.nextInt();		// Tag
@@ -29,32 +34,54 @@ public class Datumerweiterung {
 		m2 = sc.nextInt();		// Delta-Minute
 		
 
+			hochrechnenTage();
+			berechneDatum();
+			System.out.println();
+			System.out.println("Ergebnis");		
+			System.out.println(j + " " + m + " " + t + " " + s + " " + m1);
+		
+			System.out.println("Möchten sie eine neue Berechnung j/n ?");
+			eingabe = sc.next();
 				
-//		System.out.println(j + " " + m +" "+ t + " " + s + " " + m1 + " " + m2);
+		} while(eingabe.toString() == "j");
+
 		
-		
+		sc.close();	
+	}	
 	
-		// 1. Berechnen der Minute ab 1600 M1 T1 S1 M0
-		// 2. Aufaddieren der Minuten m2
-		// 3. Umrechten in das Datumsformat
-		
-		berechneMinuteabStartdatum();
+				
+	
+	private static void berechneDatum() {
+
+		while(t > monlaeng[m-1])  {
+			m++;
+			t-= monlaeng[m-2];
 			
-		
+			while(m > 12)  {
+				j++;
+				m-= 12;
+			}
+		}
+
 	}
 
-	private static void berechneMinuteabStartdatum() {
-		
-		for(int i = 1; m; i++)  {
-			
-			MinuteinJahr = MinuteimJahr + (monlaeng[i] *  
-		}
-			
-		
 
-		min = j * 
+	private static void hochrechnenTage() {
 		
-		
+		mintemp = m1 + m2;    		//Summe Minuten
+		m1 = mintemp % 60;			//Erzeugung der Rest_Minuten
+
+	
+		while (mintemp/60 > 0)  {
+			s++;
+			mintemp-= 60;
+				
+			if(s > 23)  {
+				t++;
+				s-=24;	
+			}
+		}
+
 	}
 
 }
